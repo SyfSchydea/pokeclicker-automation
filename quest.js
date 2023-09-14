8360c001ccabf6214619cc4dc90ffde9a836068b
SyfP <>
2023-09-14 18:50:52 +0100

    quest: Add basis for auto collecting quests

--- quest.js
+++ quest.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pokeclicker - Auto Quester
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Completes quests automatically.
 // @author       SyfP
 // @match        https://www.tampermonkey.net
@@ -25,9 +25,44 @@
 
 	const page = {
 		/**
-		 * Method Specification
+		 * Check if the game is loaded.
+		 *
+		 * @return - Truthy if the game has loaded, falsey if it hasn't yet.
 		 */
-		methodName() {
+		gameLoaded() {
+			return App?.game;
+		},
+
+		/**
+		 * Fetch the number of quests which the player currently has active.
+		 * This includes quests which are in progress, and ones
+		 * which are complete, but yet to be collected.
+		 *
+		 * @return {number} - Number of active quests.
+		 */
+		getActiveQuestCount() {
+			throw new Error("TODO: implement me");
+		},
+
+		/**
+		 * Check if the given active quest has been completed,
+		 * and is waiting to be collected.
+		 *
+		 * @param questIdx {number} - Index of the active quest to query.
+		 * @return                  - Truthy if the quest is completed,
+		 *                            falsey if not.
+		 */
+		activeQuestCompleted(questIdx) {
+			throw new Error("TODO: implement me");
+		},
+
+		/**
+		 * Attempt to collect a completed quest.
+		 *
+		 * @param questIdx {number} - Index of the active quest to collect.
+		 */
+		collectQuest(questIdx) {
+			throw new Error("TODO: implement me");
 		},
 	};
 
@@ -40,8 +75,36 @@
 	 * through the page interface defined above.
 	 */
 
-	function tick
+	const DELAY_INIT    =  5 * 1000;
+	const DELAY_IDLE    = 10 * 1000;
+	const DELAY_COLLECT =       500;
+
+	function collectCompletedQuest() {
+		const questCount = page.getActiveQuestCount();
+		for (let i = 0; i < questCount; ++i) {
+			if (page.activeQuestCompleted(i)) {
+				page.collectQuest(i);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	function tick() {
+		if (!page.gameLoaded()) {
+			return setTimeout(tick, DELAY_IDLE);
+		}
+
+		// TODO: Add setting if quests should be auto-completed. False by default
+		if (collectCompletedQuest()) {
+			return setTimeout(tick, DELAY_COLLECT);
+		}
+
+		setTimout(tick, DELAY_IDLE);
+	}
 
 	(function main() {
+		setTimeout(tick, DELAY_INIT);
 	})();
 })();
