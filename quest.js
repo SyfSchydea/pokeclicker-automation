0109519e849d1bb68673d05aa5569d3fbb18a926
SyfSchydea <sashapwn@gmail.com>
2023-09-14 11:07:04 +0100

    quest: Add initial WIP version of quest script

---
+++ quest.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Pokeclicker - Auto Quester
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Completes quests automatically.
+// @author       SyfP
+// @match        https://www.tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(() => {
+	"use strict";
+
+	///// Page Interface /////
+
+	/*
+	 * This section should contain functions which
+	 * provide an interface to the target page.
+	 *
+	 * The intended behaviour of these functions should be
+	 * clearly defined in comments, to make them easier to update
+	 * and maintain if and when the target page changes, as the
+	 * interface may not function correctly after any change.
+	 */
+
+	const page = {
+		/**
+		 * Method Specification
+		 */
+		methodName() {
+		},
+	};
+
+	//////////////////////////
+
+	/*
+	 * The main functionality of the script should go here.
+	 *
+	 * Any interaction with the page should only be done
+	 * through the page interface defined above.
+	 */
+
+	function tick
+
+	(function main() {
+	})();
+})();
