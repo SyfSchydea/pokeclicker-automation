3761c3df1a0b0c4698646dd5c7ffdb3897a52241
SyfP <>
2023-09-14 21:51:14 +0100

    quest: fix capitalisation in a variable

--- quest.js
+++ quest.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pokeclicker - Auto Quester
 // @namespace    http://tampermonkey.net/
-// @version      0.4.3
+// @version      0.4.4
 // @description  Completes quests automatically.
 // @author       SyfP
 // @match        https://www.tampermonkey.net
@@ -113,7 +113,7 @@
 				return {};
 			}
 
-			return JSON.parse(settingsJSON);
+			return JSON.parse(settingsJson);
 		}
 
 		get() {
