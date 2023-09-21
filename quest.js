9eb3e0fe3c9e141b69a3e1c6899181743b3124e4
SyfP <>
2023-09-21 01:07:21 +0100

    quest: Fix colon in case stmt

--- quest.js
+++ quest.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pokeclicker - Auto Quester
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.5.1
 // @description  Completes quests automatically.
 // @author       SyfP
 // @match        https://www.tampermonkey.net
@@ -127,7 +127,7 @@
 				case GainMoneyQuest:
 					return {type: QuestType.POKEDOLLARS};
 
-				default;
+				default:
 					return {type: QuestType.UNKNOWN};
 			}
 		},
