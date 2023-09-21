f8de5602c8f49486da3f37150d20474a96bb84a5
SyfP <>
2023-09-21 20:04:42 +0100

    farm-hand: bump version for quest interoperability

--- farm-hand.js
+++ farm-hand.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Poké-clicker - Better farm hands
 // @namespace    http://tampermonkey.net/
-// @version      1.38.3+quest.1
+// @version      1.39
 // @description  Works your farm for you.
 // @author       SyfP
 // @match        https://www.pokeclicker.com/
