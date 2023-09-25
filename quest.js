629d805a7a4741d7bee1144b49eeca2cbd049c73
SyfSchydea <sashapwn@gmail.com>
2023-09-25 20:09:51 +0100

    quest: Bump version for issue19

--- quest.js
+++ quest.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pokeclicker - Auto Quester
 // @namespace    http://tampermonkey.net/
-// @version      0.20+issue19
+// @version      0.20.1
 // @description  Completes quests automatically.
 // @author       SyfP
 // @match        https://www.tampermonkey.net
