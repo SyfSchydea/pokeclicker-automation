d16bfe9d715f54287931d98db988afd1148fe56b
SyfP <>
2023-09-29 18:10:20 +0100

    quest: Fix handling when a route with a given type is not found

--- quest.js
+++ quest.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pokeclicker - Auto Quester
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.1.1
 // @description  Completes quests automatically.
 // @author       SyfP
 // @match        https://www.tampermonkey.net
@@ -1423,6 +1423,10 @@
 				bestRoute = route;
 				bestFrequency = freq;
 			}
+		}
+
+		if (bestRoute == null) {
+			return null;
 		}
 
 		return new RouteLocation(bestRoute);
