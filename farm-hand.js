efac4ca636bccc3419f269478483bdd629f73052
SyfSchydea <55262569+SyfSchydea@users.noreply.github.com>
2023-10-06 13:58:57 +0100

    Farm-hand: bump version for farming liechi

--- farm-hand.js
+++ farm-hand.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Poké-clicker - Better farm hands
 // @namespace    http://tampermonkey.net/
-// @version      1.43+farm-liechi
+// @version      1.44
 // @description  Works your farm for you.
 // @author       SyfP
 // @match        https://www.pokeclicker.com/
