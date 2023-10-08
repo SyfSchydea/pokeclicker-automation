3d62b88bb63a55f03cc137447fde772340c94cb7
SyfP <>
2023-10-08 23:11:12 +0100

    farm-hand: Bumb version number for starf mutations

--- farm-hand.js
+++ farm-hand.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Poké-clicker - Better farm hands
 // @namespace    http://tampermonkey.net/
-// @version      1.44+starf-mutations
+// @version      1.45
 // @description  Works your farm for you.
 // @author       SyfP
 // @match        https://www.pokeclicker.com/
